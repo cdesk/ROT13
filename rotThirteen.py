690ea21e79e441e4c2b4d2286c6ceaa418d5eb4a
cdesk <cdesk@users.noreply.github.com>
2017-01-26 22:03:49 -0500

    Add files via upload

---
+++ rotThirteen.py
@@ -0,0 +1,36 @@
+import os
+
+import jinja2
+import webapp2
+import string
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+    autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+            self.response.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.render("rot_thirteen.html", items = items)
+
+    def post(self):
+        user_input = self.request.get('input_text')
+        trans = string.maketrans(
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+                "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm")
+        output_trans = string.translate(str(user_input), trans)
+        self.render("rot_thirteen.html", input_text = output_trans)
+
+app = webapp2.WSGIApplication([
+                ('/', MainPage)],
+                 debug=True)
